323622a13bb9ee6b5f67d38bf49343ac3833f259
IdosElmo <36295360+IdosElmo@users.noreply.github.com>
2020-11-22 15:27:37 +0200

    Add files via upload

---
+++ ex1/test.java
@@ -0,0 +1,16 @@
+package ex1;
+
+import java.util.*;
+
+public class test {
+
+	public static void main(String[] args) {
+		
+		PriorityQueue<node_info> q = new PriorityQueue<>();
+		
+		System.out.println(q.poll()); //look at first elemt in Q
+//		q.poll(); //look and REMOVE first element in Q
+		
+	}
+	
+}
